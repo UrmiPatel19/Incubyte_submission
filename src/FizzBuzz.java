cddc0f5db560ba967c2be3a8e4cbce5591b150d1
Urmi Patel <urmipatel05@gmail.com>
2020-09-21 16:54:37 +0530

    fizzbuzz_with_testcases

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -1,9 +1,27 @@
 
 public class FizzBuzz {
 
-	public static String convertInNumber(int i) {
+	public static String convertToFizzBuzz(int i) {
+		
+		if(i==0)
+			return "0";
+		
+		if(i%3==0 && i%5==0)
+		{
+			return "FizzBuzz";
+		}
+		else if(i%3==0)
+		{
+			return "Fizz";
+		}
+		else if(i%5==0)
+		{
+			return "Buzz";
+		}
 		
 		return String.valueOf(i);
 	}
+
+	
 
 }
--- src/FizzBuzzTestCases.java
+++ src/FizzBuzzTestCases.java
@@ -6,10 +6,28 @@ public class FizzBuzzTestCases {
 
 	
 	@Test
-	public void test1()
+	public void testforNormalNumber()
 	{
 		
-		assertTrue(FizzBuzz.convertInNumber(1).equals("1"));
-		assertTrue(FizzBuzz.convertInNumber(2).equals("2"));
+		assertTrue(FizzBuzz.convertToFizzBuzz(1).equals("1"));
+		assertTrue(FizzBuzz.convertToFizzBuzz(2).equals("2"));
+	}
+	@Test
+	public void testForFizz()
+	{
+		assertTrue(FizzBuzz.convertToFizzBuzz(3).equals("Fizz"));
+		assertTrue(FizzBuzz.convertToFizzBuzz(6).equals("Fizz"));
+	}
+	@Test
+	public void  testForBuzz()
+	{
+		assertTrue(FizzBuzz.convertToFizzBuzz(5).equals("Buzz"));
+		assertTrue(FizzBuzz.convertToFizzBuzz(10).equals("Buzz"));
+	}
+	@Test
+	public void testForFizzBuzz()
+	{
+		assertTrue(FizzBuzz.convertToFizzBuzz(15).equals("FizzBuzz"));
+		assertTrue(FizzBuzz.convertToFizzBuzz(30).equals("FizzBuzz"));
 	}
 }
