88d56c34ca7dcf4ce2ef621fc0ffb59f8383551f
Urmi Patel <urmipatel05@gmail.com>
2020-09-21 13:11:40 +0530

    Commit with first basic test case

---
+++ src/FizzBuzz.java
@@ -0,0 +1,9 @@
+
+public class FizzBuzz {
+
+	public static String getResult(int i) {
+		
+		return "1";
+	}
+
+}
---
+++ src/FizzBuzzTestCases.java
@@ -0,0 +1,13 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FizzBuzzTestCases {
+
+	
+	@Test
+	public void test1()
+	{
+		assertTrue(FizzBuzz.getResult(1).equals("1"));
+	}
+}
